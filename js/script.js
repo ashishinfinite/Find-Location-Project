cd0bec0aa4aa9de5fd6ef34dd5bb2b17eb91e5fe
abhishek b. jha <abhishek b. jha>
2016-10-22 05:03:13 +0530

    adding new york times news feed

--- js/script.js
+++ js/script.js
@@ -21,7 +21,20 @@ $body.append('<img class="bgimg">');
 $('.bgimg').attr("src","http://maps.googleapis.com/maps/api/streetview?size=600x400&location="+add+", "+city);
 
 
+var nytimesURL='http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+city+'&sort=newest&api-key=8af86f8919484763b710244cc768bcd1'
+$.getJSON(nytimesURL,function(data){
 
+$nytHeaderElem.text('NEW YORK TIMES ARTICLE'+city);
+articles=data.response.docs;
+for(var i=0;i<articles.length;i++){
+var article=articles[i];
+$nytElem.append('<li class="article">'+'<a href="'+article.web_url+'">'+article.headline.main+'</a>'+'<p>'+article.snippet+'</p>'+'</li>');
+
+};
+
+
+
+});
 
 
 
